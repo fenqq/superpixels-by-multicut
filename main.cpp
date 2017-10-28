d26c50a1884a440daff9acc957f0e9bfbdf9681d
Eric Kendinibilir <kendinibilir.eric@gmail.com>
2017-10-28 12:59:13 +0200

    changed behaviour of algorithm to the contrast of image

--- main.cpp
+++ main.cpp
@@ -45,7 +45,322 @@ void my_handler(int s) {
     exit(2);
   }
 }
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * Copyright (c) 2016, David Stutz
+ * Contact: david.stutz@rwth-aachen.de, davidstutz.de
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+//returns number of merged superpixels with a neighbour
+int enforceMinimumSuperpixelSize(const cv::Mat &image, cv::Mat &labels, int size) {
+    int max_label = 0;
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) > max_label) {
+                max_label = labels.at<int>(i, j);
+            }
+        }
+    }
+    
+    //std::vector<cv::Vec3b> means(max_label + 1, cv::Vec3b(0, 0, 0));
+    std::vector<float> means(max_label + 1, 0.f);
+    std::vector<int> counts(max_label + 1, 0);
+    
+    std::vector< std::vector<int> > neighbors(max_label + 1);
+    for (unsigned int k = 0; k < neighbors.size(); k++) {
+        neighbors[k] = std::vector<int>(0);
+    }
+    
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            int label = labels.at<int>(i, j);
+            /*
+            means[label][0] += image.at<cv::Vec3b>(i, j)[0];
+            means[label][1] += image.at<cv::Vec3b>(i, j)[1];
+            means[label][2] += image.at<cv::Vec3b>(i, j)[2];
+            */
+            means[label] += (float)image.at<uchar>(i, j);
+       
+            counts[label]++;
+           //kinda neighbours 
+            int neighbor_label = labels.at<int>(std::min(i + 1, labels.rows - 1), j);
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(std::max(0, i - 1), j);
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(i, std::min(j + 1, labels.cols - 1));
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(i, std::max(0, j - 1));
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+        }
+    }
+    
+    for (unsigned int k = 0; k < counts.size(); k++) {
+        if (counts[k] > 0) {
+           /* means[k][0] /= counts[k];
+            means[k][1] /= counts[k];
+            means[k][2] /= counts[k];
+            */
+            means[k] /= counts[k];
+        }
+    }
+    
+    int count = 0;
+    std::vector<int> new_labels(max_label + 1, -1);
+    
+    for (unsigned int k = 0; k < counts.size(); k++) {
+        if (counts[k] < size) {
+            
+            float min_distance = std::numeric_limits<float>::max();
+            for (unsigned int kk = 0; kk < neighbors[k].size(); kk++) {
+            /*  float distance = (means[k][0] - means[neighbors[k][kk]][0])*(means[k][0] - means[neighbors[k][kk]][0])
+                        + (means[k][1] - means[neighbors[k][kk]][1])*(means[k][1] - means[neighbors[k][kk]][1])
+                        + (means[k][2] - means[neighbors[k][kk]][2])*(means[k][2] - means[neighbors[k][kk]][2]);
+            */
 
+                float distance = (means[k] - means[neighbors[k][kk]])*(means[k] - means[neighbors[k][kk]]);
+                if (distance < min_distance && new_labels[neighbors[k][kk]] < 0) {
+                    min_distance = distance;
+                    new_labels[k] = neighbors[k][kk];
+                }
+            }
+          
+            count++;
+        }
+    }
+    
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            
+            int label = labels.at<int>(i, j);
+            if (new_labels[label] >= 0) {
+                
+                int new_label = new_labels[label];
+                while (new_labels[new_label] >= 0) {
+                    new_label = new_labels[new_label];
+                }
+                
+                labels.at<int>(i, j) = new_labels[label];
+            }
+        }
+    }
+    
+    return count;
+}
+
+int enforceMinimumSuperpixelSizeUpTo(const cv::Mat &image, cv::Mat &labels, int number) {
+    int max_label = 0;
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) > max_label) {
+                max_label = labels.at<int>(i, j);
+            }
+        }
+    }
+    
+    std::vector<cv::Vec3b> means(max_label + 1, cv::Vec3b(0, 0, 0));
+    std::vector<int> counts(max_label + 1, 0);
+    
+    std::vector< std::vector<int> > neighbors(max_label + 1);
+    for (unsigned int k = 0; k < neighbors.size(); k++) {
+        neighbors[k] = std::vector<int>(0);
+    }
+    
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            int label = labels.at<int>(i, j);
+            
+            /*
+            means[label][0] += image.at<cv::Vec3b>(i, j)[0];
+            means[label][1] += image.at<cv::Vec3b>(i, j)[1];
+            means[label][2] += image.at<cv::Vec3b>(i, j)[2];
+            */
+            means[label][0] += image.at<uchar>(i, j);
+            means[label][1] += image.at<uchar>(i, j);
+            means[label][2] += image.at<uchar>(i, j);
+            counts[label]++;
+            
+            int neighbor_label = labels.at<int>(std::min(i + 1, labels.rows - 1), j);
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(std::max(0, i - 1), j);
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(i, std::min(j + 1, labels.cols - 1));
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+            
+            neighbor_label = labels.at<int>(i, std::max(0, j - 1));
+            if (neighbor_label != label) {
+                if (std::find(neighbors[label].begin(), neighbors[label].end(), neighbor_label) == std::end(neighbors[label])) {
+                    neighbors[label].push_back(neighbor_label);
+                }
+            }
+        }
+    }
+    
+    for (unsigned int k = 0; k < counts.size(); k++) {
+        if (counts[k] > 0) {
+            means[k][0] /= counts[k];
+            means[k][1] /= counts[k];
+            means[k][2] /= counts[k];
+        }
+    }
+    
+    std::vector<int> ids(max_label + 1, 0);
+    for (unsigned int k = 0; k < ids.size(); k++) {
+        ids[k] = k;
+    }
+    
+    std::sort(ids.begin(), ids.end(), [&counts](int i, int j) {
+        return counts[i] < counts[j];
+    });
+    
+//    for (unsigned int k = 0; k < counts.size(); k++) {
+//        std::cout << ids[k] << ": " << counts[ids[k]] << std::endl;
+//    }
+    
+    int count = 0;
+    std::vector<int> new_labels(max_label + 1, -1);
+    
+    for (unsigned int k = 0; k < std::min((int) counts.size(), number); k++) {
+        int label = ids[k];
+        
+        float min_distance = std::numeric_limits<float>::max();
+        for (unsigned int kk = 0; kk < neighbors[label].size(); kk++) {
+            float distance = (means[label][0] - means[neighbors[label][kk]][0])*(means[label][0] - means[neighbors[label][kk]][0])
+                    + (means[label][1] - means[neighbors[label][kk]][1])*(means[label][1] - means[neighbors[label][kk]][1])
+                    + (means[label][2] - means[neighbors[label][kk]][2])*(means[label][2] - means[neighbors[label][kk]][2]);
+
+            if (distance < min_distance && new_labels[neighbors[label][kk]] < 0) {
+                min_distance = distance;
+                new_labels[label] = neighbors[label][kk];
+            }
+        }
+        
+        count++;
+    }
+    
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            
+            int label = labels.at<int>(i, j);
+            if (new_labels[label] >= 0) {
+                
+                int new_label = new_labels[label];
+                while (new_labels[new_label] >= 0) {
+                    new_label = new_labels[new_label];
+                }
+                std::cout << "r: " << i << " c: " << j <<  " : " << "before "  << labels.at<int>(i, j) << " after " << new_labels[label] << std::endl;
+                labels.at<int>(i, j) = new_labels[label];
+            }
+        }
+    }
+    
+    return count;
+}
+////////////////////////////////////////////////////////////////////////////////
+int count_labels(const cv::Mat& labels) {
+    int count;
+    std::set<int> set;
+    for (int i = 0; i < labels.rows; i++) {
+        for (int j = 0; j < labels.cols; j++) {
+            set.insert(labels.at<int>(i, j));
+        }
+    }
+    return set.size();
+}
+
+//kernel size must be odd
+template <typename MatType>
+double calculate_global_contrast(const cv::Mat& m, int kernel_size) {
+    cv::Mat m2;
+    m2 = m.clone();
+    cv::boxFilter(m, m2, -1, cv::Size(kernel_size, kernel_size), cv::Point(-1, -1), true, cv::BORDER_REPLICATE);
+    int rows = m.rows;
+    int cols = m.cols;
+    double tresh = 0.0;
+    for(int r = 0; r < rows; ++r) {
+        for(int c = 0; c < cols; ++c) {
+            tresh += m2.at<MatType>(r, c)/255.f;
+        }
+    }
+    return tresh / (rows*cols);
+}
+
+template <typename MatType>
+void count_jumps(const cv::Mat& m, std::vector<int>& rowcount, std::vector<int>& colcount, double tresh) {
+    int rows = m.rows;
+    int cols = m.cols;
+    for(int r = 0; r < rows; ++r) {
+        for(int c = 0; c < cols - 1; ++c) {
+            if(fabs(m.at<MatType>(r, c+1)/255.f - m.at<MatType>(r, c)/255.f) >= tresh){
+                rowcount[r]++;
+            }
+        }
+    }
+    for(int c = 0; c < cols; ++c) {
+        for(int r = 0; r < rows - 1; ++r) {
+            if(fabs(m.at<MatType>(r+1, c)/255.f - m.at<MatType>(r, c)/255.f) >= tresh){
+                colcount[c]++;
+            }
+        }
+    }
+}
 void CannyThreshold(int, void*)
 {
     cv::Mat detected_edges;
@@ -88,7 +403,9 @@ int main(int argc, char** argv) {
             ("output-file,o", po::value< std::string >()->default_value("out.jpg"), "picture to process")
             ("interactive", po::value<bool>()->default_value(true), "interactive mode")
             ("csv", po::value< std::string >()->default_value("out.csv"), "csv file name to write to")
-            ("lambda", po::value< double >() ->default_value(0.09), "parameter for multicut criterion")
+            ("lambda", po::value< double >() ->default_value(0.1), "parameter for multicut criterion")
+            ("jumps", po::value< double >() ->default_value(0.05), "the lower the number the more cuts are allowed")
+            ("enforce-size", po::value< double >() ->default_value(0.25), "post processing, relative to segment size")
         ;
 
         po::positional_options_description p;
@@ -136,6 +453,8 @@ int main(int argc, char** argv) {
             
         cv::waitKey(0);
     }
+    // do some blur
+    cv::GaussianBlur( src_gray, src_gray, cv::Size(3,3), 0);
 
 
     //build submatrix per grid segment
@@ -168,7 +487,10 @@ int main(int argc, char** argv) {
     }
 
     
-    double lambda =  vm["lambda"].as<double>();
+    double global_contrast = calculate_global_contrast<uchar>(src_gray, 5);
+    if(vm["interactive"].as<bool>()) {
+        DPRINT(global_contrast)
+    }
     std::vector<Graph*> segment_graphs(number_of_segments);
     //GRBEnv env = GRBEnv();
     std::vector<GRBEnv> envs(number_of_segments); // we need an environment per model
@@ -241,9 +563,11 @@ int main(int argc, char** argv) {
                 }
             }
 
-            obj2 *= lambda;
+            obj2 *= vm["lambda"].as<double>()*global_contrast;
             objective = obj1 - obj2;
             model.setObjective(objective, GRB_MAXIMIZE);
+
+            
 
             //initial constraints
             for(int x = 0; x < segment_size.width-1; ++x) {
@@ -261,6 +585,29 @@ int main(int argc, char** argv) {
                     model.addConstr(d_a + a_b + b_c >= c_d);
                 }
             }
+            // the <= k constraints:
+            std::vector<int> rowcount(segment_size.height, 0);
+            std::vector<int> colcount(segment_size.width, 0);
+            count_jumps<uchar>(submatrix, rowcount, colcount, vm["jumps"].as<double>()*global_contrast);
+            for(int r = 0; r < segment_size.height; ++r) {
+               GRBLinExpr sum(0);
+               for(int c = 0; c < segment_size.width-1; ++c) {
+                    GRBVar& v = g[b::edge(xy_to_index(c, r, segment_size), xy_to_index(c+1,r, segment_size), g).first].var;
+                    sum += v;
+               }
+               model.addConstr(sum <= rowcount[r]);
+            }
+            for(int c = 0; c < segment_size.width; ++c) {
+               GRBLinExpr sum(0);
+               for(int r = 0; r < segment_size.height-1; ++r) {
+                    GRBVar& v = g[b::edge(xy_to_index(c, r, segment_size), xy_to_index(c,r+1, segment_size), g).first].var;
+                    sum += v;
+               }
+               model.addConstr(sum <= colcount[c]);
+            }
+            
+
+
             #if 1
             myGRBCallback* cb = new myGRBCallback(g);
             model.setCallback(cb);
@@ -282,17 +629,17 @@ int main(int argc, char** argv) {
 
     // done
 
-    // write csv
-    cv::Mat csv_mat;
-    csv_mat.create(picture_size, CV_32SC1);
-    csv_mat = cv::Scalar(0); 
+    // write labels as csv
+    cv::Mat labels;
+    labels.create(picture_size, CV_32SC1);
+    labels = cv::Scalar(0); 
     int label_offset = 0;
 
     for(int i = 0; i < number_of_segments; ++i){
         GRBModel& model = models[i];
         Graph& g = *segment_graphs[i];
         cv::Size& segment_size = g[b::graph_bundle].size;
-        cv::Mat submatrix = csv_mat(g[b::graph_bundle].row, g[b::graph_bundle].col);
+        cv::Mat submatrix = labels(g[b::graph_bundle].row, g[b::graph_bundle].col);
         int max_label = 0;
         for(int x = 0; x < segment_size.width; ++x) {
           for(int y = 0; y < segment_size.height; ++y) {
@@ -301,13 +648,19 @@ int main(int argc, char** argv) {
              submatrix.at<int>(y, x) = label + label_offset;
           }
         }
-        label_offset = label_offset + max_label;
+        label_offset = label_offset + max_label + 1;//right?
     }
     std::cout << "num labels: " << label_offset << std::endl;
-    saveMatToCsv(csv_mat, vm["csv"].as<std::string>());
+    enforceMinimumSuperpixelSize(src_gray, labels, (int)(vm["enforce-size"].as<double>()*segment_size_approx.width*segment_size_approx.height)); 
+//  enforceMinimumSuperpixelSizeUpTo(src_gray, labels, 3320);
+    // ... maybe just fuse them to segment?
+    int count = count_labels(labels);
+    std::cout << "num labels after enforcing sp size: " << count << std::endl;
+    saveMatToCsv(labels, vm["csv"].as<std::string>());
 
 
     // write image
+    #if 0
     cv::Mat red;
     cv::Mat border_mask;
     border_mask.create(picture_size, CV_8UC1);
@@ -346,6 +699,27 @@ int main(int argc, char** argv) {
           }
         }
         #endif
+    }
+    #endif
+    //use labels instead:
+    cv::Mat red;
+    cv::Mat border_mask;
+    border_mask.create(picture_size, CV_8UC1);
+    border_mask = cv::Scalar(0); 
+
+    for(int r = 0; r < picture_size.height; ++r) {
+        for(int c = 0; c < picture_size.width-1; ++c) {
+            if(labels.at<int>(r, c) != labels.at<int>(r, c+1)) {
+                border_mask.at<uchar>(r, c) = 1;
+            }
+        }
+    }
+    for(int r = 0; r < picture_size.height-1; ++r) {
+        for(int c = 0; c < picture_size.width; ++c) {
+            if(labels.at<int>(r, c) != labels.at<int>(r+1, c)) {
+                border_mask.at<uchar>(r, c) = 1;
+            }
+        }
     }
     red.create(picture_size, CV_8UC3);
     red = cv::Scalar(0, 0, 255);
